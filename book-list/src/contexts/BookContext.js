1fa1664e5fda8e8ee3a774b42373fe9063123de4
zolicoeur <ci_attatra@esi.dz>
2020-05-27 15:57:27 +0200

    use reducer with context

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -1,27 +1,13 @@
-import React ,{createContext, useState} from 'react';
-import { uuid } from 'uuidv4';
+import React ,{createContext, useReducer} from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books,setBooks]=useState([
-        { id: 1, title: 'first title', author:'first author'},
-        { id: 2, title: 'second title', author:'second author'},
-        { id: 3, title: 'third title', author:'third author'},
-    ])
-    const addBook=(title,author)=>{
-        console.log(title,author)
-        setBooks([...books,{ id: uuid(),title,author}])
-        console.log(books)
-    }
-    const removeBook=(id)=>{
-        setBooks(
-            books.filter(book=>book.id!==id)
-        )
-    }
+    const [books,dispatch]=useReducer(bookReducer,[])
     //props.children represents the component that the provider is going to wrap
     return ( 
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books,dispatch}}>
             {props.children}
         </BookContext.Provider>
      );
